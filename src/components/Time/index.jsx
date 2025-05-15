45fa35b6b907fb3d0b30938fb76c1651635f25dc
Antoniel Aureliano <tonielaureliano@gmailcom>
2025-05-15 16:15:30 -0300

    feat(play-button): create PlayButton component.

--- src/components/Time/index.jsx
+++ src/components/Time/index.jsx
@@ -1,5 +1,8 @@
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import { PlayButton } from "../PlayButton";
+
 import 'react-circular-progressbar/dist/styles.css';
+import styles from './Time.module.css';
 
 const red = '#f54e4e';
 //const green = '#4aec8c';
@@ -18,8 +21,8 @@ export const Time = () => {
                     trailColor: 'rgba(255, 255, 255, 0.2)',
                 })} 
             />
-            <div>
-
+            <div className={styles.timeButton}>
+                <PlayButton />
             </div>
 
         </div>
