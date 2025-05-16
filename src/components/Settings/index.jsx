6c64288a852935e18d8de52ce8559e211eaf98b7
Antoniel Aureliano <tonielaureliano@gmail.com>
2025-05-16 11:11:00 -0300

    feat(settings): create Settings component with module CSS.

--- src/components/Settings/index.jsx
+++ src/components/Settings/index.jsx
@@ -5,6 +5,24 @@ import styles from "./Settings.module.css";
 export const Settings = () => {
 
     return (
-        <div>Settings</div>
+        <div className={styles.settingsContent}>
+            <label>Work:</label>
+            <ReactSlider
+                className={`${styles.slider} ${styles.sliderWork}`}    
+                thumbClassName={styles.thumb}
+                value={45}
+                min={1}
+                max={120}
+            />
+            
+            <label>Break:</label>
+            <ReactSlider
+                className={`${styles.slider} ${styles.sliderBreak}`}    
+                thumbClassName={styles.thumb}
+                value={15}
+                min={1}
+                max={120}
+            />
+        </div>
     )
 }
